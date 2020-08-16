57f647cd7839702ec8405c850991413cb29c0989
Islam Attrash <isattrash@gmail.com>
2020-08-16 19:13:17 +0300

    v1

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
 declare module 'golden-path' {
-    // TODO
+    export const get = (path: string, object: Object|Array) => any;
+    export const update = (path: string, newValue: any, object: Object|Array) => any;
+    export const v = (value: any) => any;
 }
