49b30cf8d28b2bb80d97c2f50872aa599dc5b282
Islam Attrash <isattrash@gmail.com>
2021-07-08 18:48:28 +0300

    [FIX] Improve errors logging

--- src/index.js
+++ src/index.js
@@ -3,39 +3,47 @@ import resolvePath from './resolvePath';
 import { TOKEN_HASH } from './constants';
 
 const update = curry((unResolvedPath, value, object) => {
-    let { path, paths, notExist } = resolvePath(unResolvedPath, object);
-    if (notExist) { return object; }
-    if (path && !paths) { paths = [path]; }
+    try {
+        let { path, paths, notExist } = resolvePath(unResolvedPath, object);
+        if (notExist) { return object; }
+        if (path && !paths) { paths = [path]; }
 
-    let objectResult = object;
+        let objectResult = object;
 
-    let newVal = value;
+        let newVal = value;
 
-    if (paths) {
-        paths.forEach((p) => {
-            if (typeof value === 'function') {
-                newVal = value(
-                    rPath(p, object)
-                );
-            }
+        if (paths) {
+            paths.forEach((p) => {
+                if (typeof value === 'function') {
+                    newVal = value(
+                        rPath(p, object)
+                    );
+                }
 
-            const isSameValue = rPath(p, objectResult) === newVal;
-            if (isSameValue) { return; }
-        
-            objectResult = assocPath(p, newVal, objectResult)
-        });
+                const isSameValue = rPath(p, objectResult) === newVal;
+                if (isSameValue) { return; }
+            
+                objectResult = assocPath(p, newVal, objectResult)
+            });
+        }
+
+        return objectResult;
+    } catch (err) {
+        throw new Error(`Golden Path::Error updating the path "${unResolvedPath}" with the value "${value}"`);
     }
-
-    return objectResult;
 });
 
 const get = curry((unResolvedPath, object) => {
-    let { path, paths, notExist, isGreedy } = resolvePath(unResolvedPath, object);
-    if (notExist) { return isGreedy ? [] : undefined; }
-
-    if (path) { return rPath(path, object); }
-
-    if (paths) { return paths.map((p) => rPath(p, object)); }
+    try {
+        let { path, paths, notExist, isGreedy } = resolvePath(unResolvedPath, object);
+        if (notExist) { return isGreedy ? [] : undefined; }
+    
+        if (path) { return rPath(path, object); }
+    
+        if (paths) { return paths.map((p) => rPath(p, object)); }
+    } catch (err) {
+        throw new Error(`Golden Path::Error getting the path "${unResolvedPath}"`);
+    }
 });
 
 const v = (value) => `${TOKEN_HASH}${value}${TOKEN_HASH}`;
